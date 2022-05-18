d61109ba5a046f322b6792b246a112e98de20bf3
panZoubek <98482979+panZoubek@users.noreply.github.com>
2022-05-18 18:06:08 +0200

    Update lyricsspambot.py

--- lyricsspambot.py
+++ lyricsspambot.py
@@ -5,21 +5,26 @@ count = 0
 
 
 print("How many times do you want to reapeat?:")
-
 ttp = input()
-
 trr = int(ttp)
 
 time.sleep(1)
 
-print("5 seconds to proceed")
 
-time.sleep(5)
+print("How many seconds do I have to wait?")
+tmslp = input()
+tmslp = int(tmslp)
+
+
+print(tmslp, "seconds to proceed")
+
+time.sleep(tmslp)
+
 
 for x in range(trr):
-	count += 1
-	for word in f:
-		time.sleep(1.6)
-		pyautogui.typewrite(word)
-		pyautogui.press("enter")
-	print("Times done:", count)
+    count += 1
+    for word in f:
+        time.sleep(1.6)
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+    print("Times done:", count)
